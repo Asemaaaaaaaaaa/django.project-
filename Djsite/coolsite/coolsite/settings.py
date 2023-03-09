72808562dfcf193dcfe97f072d07fb0f64330531
Asemaaaaaaaaaa <assem.syrymbetova@narxoz.kz>
2023-03-09 09:12:28 +0600

    fa7 init

--- Djsite/coolsite/coolsite/settings.py
+++ Djsite/coolsite/coolsite/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-#k_+3vb#jfwn18$)5m-!=lj(z*j)r_(0lb%(wg4+tu#i*d&#%d'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1']
 
@@ -117,6 +117,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = []
+
+
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = 'media/'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
--- Djsite/coolsite/pyproject/models.py
+++ Djsite/coolsite/pyproject/models.py
@@ -1,9 +1,6 @@
 from django.db import models
 from django.urls import reverse
 
-
-
-
 class Books(models.Model):
     title = models.CharField(max_length=255)
     author = models.TextField()
@@ -60,5 +57,4 @@ class Training1(models.Model):
     training_manager = models.CharField(max_length=255)
     photo = models.ImageField(upload_to="photos/%y/%m/%d/")
 
-    def __str__(self):
-        return self.title
+
