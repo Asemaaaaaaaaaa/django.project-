1870adf3f0baf8f56fcfcb238641c37710c8fd3c
Asemaaaaaaaaaa <assem.syrymbetova@narxoz.kz>
2023-03-09 09:13:01 +0600

    fa7 init

--- Djsite/coolsite/pyproject/urls.py
+++ Djsite/coolsite/pyproject/urls.py
@@ -1,10 +1,14 @@
 from django.urls import path, re_path
 from .views import *
 urlpatterns = [
-    path('', index, name='home'),
-    path('training1/', training1, name='training1'),
+
+    path('',index,name='home'),
+    path('trainingSelfDevelopment/',training1,name='training1'),
+    path('trainingManager1/',training_manager1,name='training_manager1'),
+    path('trainingPsychology',training2,name='training2'),
+    path('books',books,name='books'),
+    path('category/<int:category_id>/',show_category,name='category'),
+    path('post/<int:post_id>/', show_post, name='post')
+
 
 ]
-
-handler404 = pageNotFound
-
--- Djsite/coolsite/pyproject/views.py
+++ Djsite/coolsite/pyproject/views.py
@@ -1,40 +1,46 @@
-from django.http import HttpResponse, HttpResponseNotFound, Http404
+from django.http import HttpResponse, HttpResponseNotFound, HttpResponseForbidden, HttpResponseServerError, \
+    HttpResponseBadRequest
 from django.shortcuts import render, redirect
 
-from.models import *
-
-
-menu = ["Тренинги", "Психология", "Саморазвития", "Мастер-классы"]
+from .models import *
+# Create your views here.
 def index(request):
-
-    return render(request, 'pyproject/index.html',{ 'title': 'Главная страница'})
+    return render(request,'pyproject/index.html')
 
 def training1(request):
     t1 = Training1.objects.all()
-    return render(request, 'pyproject/training1.html', {'t1': t1})
-
+    return render(request,'pyproject/training1.html',{'t1':t1})
+def training_manager1(request):
+    tr1 = Training_manager1.objects.all()
+    return render(request,'pyproject/training_manager1.html',{'tr1':tr1})
 def training2(request):
     t2 = Training2.objects.all()
-    return render(request, 'pyproject/training2.html', {'t2': t2})
-
-
+    return render(request,'pyproject/training2.html',{'t2':t2})
 def books(request):
     b = Books.objects.all()
     categories = Category.objects.all()
-    return render(request,'pyproject/books.html',{'b': b ,'categories': categories , 'category_selected':0 })
+    return render(request,'pyproject/books.html',{'b':b,'categories': categories ,'category_selected':0 })
 
-    def show_category(request, category_id):
-        b = Books.objects.filter(category_id=category_id)
-        categories = Category.objects.all()
-        return render(request, 'pyproject/books.html',
-                      {'b': b, 'categories': categories, 'category_selected': category_id})
+def show_category(request,category_id):
+    b=Books.objects.filter(category_id=category_id)
+    categories = Category.objects.all()
+    return render(request, 'pyproject/books.html', {'b': b, 'categories': categories, 'category_selected': category_id})
 
-    return HttpResponse(f"<h1>Страница категория</h1><p>{catid}</p>")
 
-def archive(request, year):
-    if int(year) > 2020:
-        return redirect('home', permanent=False)
-
-    return HttpResponse(f"<h1>Архив по годам</h1><p>{year}</p>")
 def pageNotFound(request,exception):
     return HttpResponseNotFound('<h1>Страница не найдена </h1>')
+
+
+def forbidden(request,exception):
+    return HttpResponseForbidden('<h1>Доступ запрещен </h1>')
+
+
+def serverError(exception):
+    return HttpResponseServerError('<h1>Ошибка сервера</h1>')
+
+
+def badRequest(request,exception):
+    return HttpResponseBadRequest('<h1>Неверный запрос </h1>')
+
+def show_post(request, post_id):
+     return HttpResponse(f"Отображение статьи с id = {post_id}")
